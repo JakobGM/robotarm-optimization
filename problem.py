f9955e804c8debabc435e000ab6d02f11c3d3d47
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-05-05 19:29:12 +0200

    Change objective generators to handle thetas as long vector

--- problem.py
+++ problem.py
@@ -6,129 +6,42 @@ perform thetas.reshape(n*s, 1) instead.
 """
 import numpy as np
 
-from constraints import generate_constraints_function
+from constraints import (
+    generate_constraints_function,
+    generate_constraint_gradients_function,
+)
 
+def generate_objective_function(robot_arm):
+    n = robot_arm.n
+    s = robot_arm.s
 
-def objective(thetas):
-    assert isinstance(thetas, np.ndarray)
-    rotated = np.roll(thetas.copy(), shift=-1, axis=1)
-    deltas = rotated - thetas
-    return 0.5 * np.sum(deltas ** 2)
+    def objective(thetas):
+        if not thetas.shape == (n*s,):
+            raise ValueError('Thetas not given as a single 1D-vetor, but as: ' + str(thetas.shape))
 
+        rotated = np.roll(thetas.copy().reshape((n, s), order='F'), shift=-1, axis=1)
+        deltas = rotated - thetas.reshape((n, s), order='F')
+        return 0.5 * np.sum(deltas ** 2)
 
-def constraint(thetas, lengths, constraint_number, coordinate):
-    n = len(lengths)
-    col_index = None
-    if constraint_number % 2 == 1:
-        col_index = (constraint_number + 1) // 2 - 1
-    elif constraint_number % 2 == 0:
-        col_index = constraint_number // 2 - 1
-    theta_cum_sum = np.cumsum(thetas[:, col_index])
-    constraint_value = 0
-    for i in range(n):
-        if constraint_number % 2 == 0:
-            constraint_value += lengths[i] * np.sin(theta_cum_sum[i])
-        elif constraint_number % 2 == 1:
-            constraint_value += lengths[i] * np.cos(theta_cum_sum[i])
-    return constraint_value - coordinate
+    return objective
 
+def generate_objective_gradient_function(robot_arm):
+    n = robot_arm.n
+    s = robot_arm.s
 
-def objective_gradient(thetas):
-    assert isinstance(thetas, np.ndarray)
-    n, s = thetas.shape
-    objective_gradient_matrix = np.zeros((n, s))
-    for j in range(s):
-        for i in range(n):
-            if (j > 0) and j < (s - 1):
-                objective_gradient_matrix[i, j] = 2 * thetas[i, j] - (thetas[i, j - 1] + thetas[i, j + 1])
-            elif j == 0:
-                objective_gradient_matrix[i, j] = 2 * thetas[i, j] - (thetas[i, s - 1] + thetas[i, 1])
-            elif j == s - 1:
-                objective_gradient_matrix[i, j] = 2 * thetas[i, j] - (thetas[i, s - 2] + thetas[i, 0])
-    return objective_gradient_matrix
+    def objective_gradient(thetas):
+        if not thetas.shape == (n*s,):
+            raise ValueError('Thetas not given as a single 1D-vetor, but as: ' + str(thetas.shape))
+        else:
+            thetas = thetas.reshape((n, s), order='F')
 
+        def roll(x, y): np.roll(x.copy(), shift=y, axis=1)
 
-def constraint_gradient(thetas, lengths, constraint_number):
-    assert isinstance(thetas, np.ndarray)
-    n, s = thetas.shape
-    col_index = None
-    assert n == len(lengths)
-    constraint_gradient_matrix = np.zeros((n, s))
-    if constraint_number % 2 == 1:
-        col_index = (constraint_number + 1) // 2 - 1
-    elif constraint_number % 2 == 0:
-        col_index = constraint_number // 2 - 1
-    theta_cum_sum = np.cumsum(thetas[:, col_index])
-    for i in range(n):
-        elements_in_outer_sum = np.zeros(n)
-        for j in range(n):
-            if constraint_number % 2 == 1:
-                elements_in_outer_sum[j] = - lengths[j] * np.sin(theta_cum_sum[j])
-            elif constraint_number % 2 == 0:
-                elements_in_outer_sum[j] = lengths[j] * np.cos(theta_cum_sum[j])
-        constraint_gradient_matrix[i, col_index] = np.sum(elements_in_outer_sum[i:])
-    return constraint_gradient_matrix
+        return (2*thetas - roll(thetas, -1) - roll(thetas, 1)).reshape((n*s,), order='F')
 
+    return objective_gradient
 
-def constraint_squared_gradient(thetas, lengths, constraint_number):
-    n, s = thetas.shape
-    col_index = None
-    assert n == len(lengths)
-    constraint_gradient_matrix = np.zeros((n, s))
-    if constraint_number % 2 == 1:
-        col_index = (constraint_number + 1) // 2 - 1
-    elif constraint_number % 2 == 0:
-        col_index = constraint_number // 2 - 1
-    theta_cum_sum = np.cumsum(thetas[:, col_index])
-    final_sum = np.zeros(n)
-    for row_index in range(n):
-        partial_theta_sum = np.sum(thetas[row_index:, col_index])
-        for i in range(n):
-            for j in range(n):
-                l_prod = lengths[i] * lengths[j]
-                if constraint_number % 2 == 1:
-                    if i < row_index <= j:
-                        final_sum[row_index] += - l_prod * np.cos(theta_cum_sum[i]) * np.sin(partial_theta_sum)
-                    elif j < row_index <= i:
-                        final_sum[row_index] += - l_prod * np.sin(partial_theta_sum) * np.cos(theta_cum_sum[j])
-                    elif i >= row_index and j >= row_index:
-                        final_sum[row_index] += - l_prod * (np.sin(theta_cum_sum[i]) * np.cos(theta_cum_sum[j]) +
-                                                            np.cos(theta_cum_sum[i]) * np.sin(theta_cum_sum[j]))
-                elif constraint_number % 2 == 0:
-                    if i < row_index <= j:
-                        final_sum[row_index] += l_prod * np.sin(theta_cum_sum[i]) * np.cos(partial_theta_sum)
-                    elif j < row_index <= i:
-                        final_sum[row_index] += l_prod * np.cos(partial_theta_sum) * np.sin(theta_cum_sum[j])
-                    elif i >= row_index and j >= row_index:
-                        final_sum[row_index] += l_prod * (np.cos(theta_cum_sum[i]) * np.sin(theta_cum_sum[j]) +
-                                                          np.sin(theta_cum_sum[i]) * np.cos(theta_cum_sum[j]))
-    constraint_gradient_matrix[:, col_index] = final_sum
-    return constraint_gradient_matrix
-
-
-def constraint_grad_set(thetas, lengths):
-    n, s = thetas.shape
-    constraint_set = np.zeros((n * s, 2 * s))
-    for i in range(1, 2 * s + 1):
-        constraint_grad = constraint_gradient(thetas, lengths, i).flatten('F').reshape((n * s,))
-        constraint_set[:, i - 1] = constraint_grad
-    return constraint_set
-
-
-def get_constraint_set(thetas, lengths, coordinates):
-    n, s = thetas.shape
-    constraint_set = np.zeros((2, s))
-    for i in range(1, 2 * s + 1):
-        if i % 2 == 0:
-            col_index = i // 2 - 1
-            constraint_set[1, col_index] = constraint(thetas, lengths, i, coordinates[1, col_index])
-        elif i % 2 == 1:
-            col_index = (i + 1) // 2 - 1
-            constraint_set[0, col_index] = constraint(thetas, lengths, i, coordinates[0, col_index])
-    return constraint_set.reshape((2 * s,))
-
-
-def generate_quadratically_penealized_objective(robot_arm):
+def generate_quadratically_penalized_objective(robot_arm):
     '''
     Given a RobotArm object with a valid joint lenghts and destinations
     this function returns a quadratically penalized objective function
@@ -137,9 +50,10 @@ def generate_quadratically_penealized_objective(robot_arm):
     '''
     n = robot_arm.n
     s = robot_arm.s
+    objective = generate_objective_function(robot_arm)
     constraints_func = generate_constraints_function(robot_arm)
 
-    def quadratically_penealized_objective(thetas, mu):
+    def quadratically_penalized_objective(thetas, mu):
         if not thetas.shape == (n * s,):
             raise ValueError('Thetas is not given as 1D-vector, but as: ' + \
                              str(thetas.shape))
@@ -147,7 +61,8 @@ def generate_quadratically_penealized_objective(robot_arm):
         return objective(thetas) + \
                0.5 * mu * np.sum(constraints_func(thetas) ** 2)
 
-    return quadratically_penealized_objective
+
+    return quadratically_penalized_objective
 
 
 def generate_quadratically_penalized_objective_gradient(robot_arm):
@@ -156,6 +71,10 @@ def generate_quadratically_penalized_objective_gradient(robot_arm):
     constraints_func = generate_constraints_function(robot_arm)
 
     def quadratically_penalized_objective_gradient(thetas, mu):
+        if not thetas.shape == (n * s,):
+            raise ValueError('Thetas is not given as 1D-vector, but as: ' + \
+                             str(thetas.shape))
+
         grad = objective_gradient(thetas).flatten('F')
         for constraint_number in range(2 * s):
             grad += 0.5 * mu * constraints_func[constraint_number](thetas).flatten('F')
--- tests/test_problem.py
+++ tests/test_problem.py
@@ -1,144 +1,36 @@
 import unittest
 import numbers
 import numpy as np
-from problem import (objective, objective_gradient,
-                     constraint, constraint_gradient, constraint_squared_gradient, constraint_grad_set,
-                     generate_quadratically_penealized_objective,
-                     generate_quadratically_penalized_objective_gradient,)
+from problem import (
+    generate_objective_function,
+    generate_quadratically_penalized_objective,
+    generate_quadratically_penalized_objective_gradient,
+)
 from robot_arm import RobotArm
-
 
 class TestObjectiveFunction(unittest.TestCase):
     def setUp(self):
-        self.thetas = np.array(((0.5, 2, 3,), (4, 5, 6,),))
+        self.thetas = np.array((0.5, 4, 2, 5, 3, 6,))
         self.thetas.setflags(write=False)
+        lengths = (1, 1,)
+        destinations = ((0, 0, 0), (0, 0, 0))
+        self.robot_arm = RobotArm(lengths, destinations)
+        self.objective = generate_objective_function(self.robot_arm)
 
     def test_theta_size(self):
-        self.assertEqual(self.thetas.shape, (2, 3))
+        self.assertEqual(self.thetas.shape, (2*3,))
 
     def test_return_type(self):
-        self.assertIsInstance(objective(self.thetas), numbers.Number)
+        self.assertIsInstance(self.objective(self.thetas), numbers.Number)
 
     def test_correct_objective_value(self):
-        self.assertEqual(objective(self.thetas), 31 / 4)
+        self.assertEqual(self.objective(self.thetas), 31 / 4)
 
     def test_immutability(self):
         original = self.thetas
-        objective(self.thetas)
+        self.objective(self.thetas)
         np.testing.assert_equal(original, self.thetas)
 
-
-class TestConstraintFunction(unittest.TestCase):
-    def setUp(self):
-        self.thetas = np.array(((0.5, 2, 3,), (4, 5, 6,),))
-        self.lengths = np.array((3, 2,))
-        self.n, self.s = self.thetas.shape
-        self.constraint_set = np.arange(1, 2 * self.s + 1, dtype=int)
-        self.coordinates = np.array(((3, 1, 4), (1, 2, 3)))
-
-    def test_theta_size(self):
-        self.assertEqual(self.thetas.shape, (2, 3))
-
-    def test_lengths_size(self):
-        self.assertEqual(self.lengths.shape, (2,))
-
-    def test_constraint_set_size(self):
-        self.assertEqual(self.constraint_set.shape, (2 * self.s,))
-
-    def test_return_type(self):
-        self.assertIsInstance(constraint(self.thetas, self.lengths, self.constraint_set[4], self.coordinates[0, 2]),
-                              numbers.Number)
-
-    def test_correct_constraint_value(self):
-        correct_value = 3 * np.cos(3) + 2 * np.cos(3 + 6) - 4
-        self.assertEqual(constraint(self.thetas, self.lengths, self.constraint_set[4], self.coordinates[0, 2]),
-                         correct_value)
-
-
-class TestObjectiveGradientFunction(unittest.TestCase):
-    def setUp(self):
-        self.thetas = np.array(((0.5, 2, 3,), (4, 5, 6,),))
-        self.thetas.setflags(write=False)
-
-    def test_theta_size(self):
-        self.assertEqual(self.thetas.shape, (2, 3))
-
-    def test_return_size(self):
-        self.assertEqual(objective_gradient(self.thetas).shape, (2, 3))
-
-    def test_correct_objective_gradient_value(self):
-        objective_gradient_matrix = np.array(((-4, 1 / 2, 7 / 2,), (-3, 0, 3,),))
-        np.testing.assert_equal(objective_gradient(self.thetas), objective_gradient_matrix)
-
-
-class TestConstraintGradientFunction(unittest.TestCase):
-    def setUp(self):
-        self.thetas = np.array(((0.5, 2, 3,), (4, 5, 6,),))
-        self.lengths = np.array((3, 2,))
-        self.n, self.s = self.thetas.shape
-        self.constraint_set = np.arange(1, 2 * self.s + 1, dtype=int)
-
-    def test_n_and_s(self):
-        self.assertEqual(self.n, self.lengths.shape[0])
-        self.assertEqual(self.s, self.thetas.shape[1])
-
-    def test_theta_size(self):
-        self.assertEqual(self.thetas.shape, (2, 3))
-
-    def test_lengths_size(self):
-        self.assertEqual(self.lengths.shape, (2,))
-
-    def test_constraint_set_size(self):
-        self.assertEqual(self.constraint_set.shape, (2 * self.s,))
-
-    def test_return_size(self):
-        self.assertEqual(constraint_gradient(self.thetas, self.lengths, self.constraint_set[4]).shape, (2, 3))
-
-    def test_correct_constraint_gradient_value(self):
-        correct_value = -(3 * np.sin(3) + 2 * np.sin(3 + 6))
-        self.assertEqual(constraint_gradient(self.thetas, self.lengths, self.constraint_set[4])[0, 2], correct_value)
-
-
-class TestConstraintSquaredGradientFunction(unittest.TestCase):
-    def setUp(self):
-        self.thetas = np.array(((0.5, 2, 3,), (4, 5, 6,),))
-        self.lengths = np.array((3, 2,))
-        self.n, self.s = self.thetas.shape
-        self.constraint_set = np.arange(1, 2 * self.s + 1, dtype=int)
-
-    def test_n_and_s(self):
-        self.assertEqual(self.n, self.lengths.shape[0])
-        self.assertEqual(self.s, self.thetas.shape[1])
-
-    def test_theta_size(self):
-        self.assertEqual(self.thetas.shape, (2, 3))
-
-    def test_lengths_size(self):
-        self.assertEqual(self.lengths.shape, (2,))
-
-    def test_constraint_set_size(self):
-        self.assertEqual(self.constraint_set.shape, (2 * self.s,))
-
-    def test_return_size(self):
-        self.assertEqual(constraint_squared_gradient(self.thetas, self.lengths, self.constraint_set[4]).shape,
-                         (2, 3))
-
-    def test_correct_constraint_squared_gradient_value(self):
-        correct_value = -(
-            3 * 3 * (2 * np.sin(3) * np.cos(3)) + 3 * 2 * (np.sin(3) * np.cos(3 + 6) + np.cos(3) * np.sin(3 + 6)) +
-            2 * 3 * (np.sin(3 + 6) * np.cos(3) + np.cos(3 + 6) * np.sin(3)) + 2 * 2 * (
-                2 * np.sin(3 + 6) * np.cos(3 + 6)))
-        self.assertEqual(constraint_squared_gradient(self.thetas, self.lengths, self.constraint_set[4])[0, 2],
-                         correct_value)
-
-
-class TestConstraintGradientSet(unittest.TestCase):
-    def setUp(self):
-        self.thetas = np.array(((0.5, 2, 3,), (4, 5, 6,),))
-        self.lengths = np.array((3, 2,))
-
-    def test_set_size(self):
-        self.assertEqual(constraint_grad_set(self.thetas, self.lengths).shape, (6, 6))
 
 class TestGeneratorFunctions(unittest.TestCase):
     def setUp(self):
@@ -151,7 +43,7 @@ class TestGeneratorFunctions(unittest.TestCase):
         self.robot_arm = RobotArm(lengths, destinations, theta)
 
     def test_penalized_objective_generator_invocation(self):
-        quad_func = generate_quadratically_penealized_objective(self.robot_arm)
+        generate_quadratically_penalized_objective(self.robot_arm)
 
     def test_penalized_objective_gradient_generator_invocation(self):
-        quad_gradient_func = generate_quadratically_penalized_objective_gradient(self.robot_arm)
+        generate_quadratically_penalized_objective_gradient(self.robot_arm)
