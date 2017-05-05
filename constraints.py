93056c7820b0f0a90cb4bf37c59164cef67b2806
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-05-05 15:16:59 +0200

    Add function for gradients

--- constraints.py
+++ constraints.py
@@ -24,3 +24,33 @@ def generate_constraints_function(robot_arm):
         return (positions - robot_arm.destinations).reshape((2*s,), order='F')
 
     return constraints
+
+
+def generate_constraint_gradients_function(robot_arm):
+    n = robot_arm.n
+    s = robot_arm.s
+
+    def constraint_gradients(thetas):
+        # The function must map from R^(ns) --> R^(ns x 2s)
+        if not thetas.shape == (n * s,):
+            raise ValueError('Thetas is not given as 1D vector')
+
+        joint_angles = np.cumsum(thetas.reshape((n, s)), axis=0)
+        assert joint_angles.shape == (n, s,)
+        x_components = robot_arm.lengths.reshape(n, 1) * np.cos(joint_angles)
+        y_components = robot_arm.lengths.reshape(n, 1) * np.sin(joint_angles)
+
+        def a(q, j): return np.sum(x_components[q:, j], dtype=np.float)
+
+        def b(q, j): return np.sum(y_components[q:, j], dtype=np.float)
+
+        gradients_matrix = np.zeros((n*s, 2*s))
+
+        for j in range(0, s):  # Constraint number
+            for q in range(0, n):   # Joint angle number
+                gradients_matrix[j*n + q, 2*j] = -b(q, j)
+                gradients_matrix[j*n + q, 2*j+1] = a(q, j)
+
+        return gradients_matrix
+
+    return constraint_gradients
--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -1,10 +1,11 @@
 import numpy as np
 import unittest
 
-from constraints import generate_constraints_function
+from constraints import (generate_constraints_function,
+                         generate_constraint_gradients_function,)
 from robot_arm import RobotArm
 
-class TestConstraintFunction(unittest.TestCase):
+class TestConstraintFunctions(unittest.TestCase):
     def setUp(self):
         self.lengths = (3, 2, 2,)
         self.destinations = (
@@ -14,8 +15,14 @@ class TestConstraintFunction(unittest.TestCase):
         self.theta = (np.pi, np.pi / 2, 0,)
         self.thetas = np.ones((3 * 5,))
         self.robot_arm = RobotArm(self.lengths, self.destinations, self.theta)
-        self.constraint_func = generate_constraints_function(self.robot_arm)
+        self.constraints_func = generate_constraints_function(self.robot_arm)
+        self.constraint_gradients_func = generate_constraint_gradients_function(self.robot_arm)
 
     def test_constraints_func_return_type(self):
-        constraints = self.constraint_func(self.thetas)
-        self.assertEquals(constraints.shape, (2*5,))
+        constraints = self.constraints_func(self.thetas)
+        self.assertEqual(constraints.shape, (2*5,))
+
+    def test_constraint_gradients_func_return_type(self):
+        constraint_gradients = self.constraint_gradients_func(self.thetas)
+        self.assertEqual(constraint_gradients.shape, (3*5, 2*5))
+        #print(np.array2string(constraint_gradients, max_line_width=np.inf))
