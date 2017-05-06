acd233c4a49e680aaeaa11a3db5d338b188ddaa5
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2017-05-06 18:43:36 +0200

    Implemented a working augmented lagrangian method, but without a final convergence test.

--- augmented_lagrangian.py
+++ augmented_lagrangian.py
@@ -1,58 +1,80 @@
 import numpy as np
 
+from constraints import generate_constraints_function, generate_constraint_gradients_function
 from methods import BFGS
 from plotting import path_figure
-from problem import objective, objective_gradient
+from problem import generate_objective_gradient_function, generate_objective_function
+from robot_arm import RobotArm
 
 
-def augmented_lagrangian_method(initial_penalty, initial_tolerance, initial_thetas,
-                                initial_lambdas, max_iter, global_tolerance, constraint, constraint_grad, robot):
-    n, s = initial_thetas.shape
-    current_lambdas = initial_lambdas
-    current_tolerance = initial_tolerance
-    current_penalty = initial_penalty
-    current_thetas = initial_thetas.flatten('F')
-    for current_iterate in range(max_iter):
-        l_c_sum = 0
-        c_sq_sum = 0
-        c_sq_sum_alternative = np.zeros(n * s)
-        for i in range(1, 2 * s + 1):
-            current_constraint = constraint(current_thetas)[i - 1]
-            current_constraint_grad = constraint_grad(current_thetas)[:, i - 1]
-            l_c_sum += current_lambdas[i - 1] * current_constraint
-            c_sq_sum += current_constraint ** 2
-            c_sq_sum_alternative += (current_lambdas[
-                                         i - 1] - current_penalty * current_constraint) * current_constraint_grad
+def generate_lagrange_multiplier(robot_arm):
+    constraints_function = generate_constraints_function(robot_arm)
 
-        def augmented_lagrangian(func1_thetas):
-            return objective(
-                func1_thetas.reshape((n, s), order='F')).flatten('F') - l_c_sum + current_penalty / 2 * c_sq_sum
+    def lagrange_multiplier(thetas, old_lagrange_multiplier, mu):
+        return old_lagrange_multiplier - mu * constraints_function(thetas)
 
-        def grad_augmented_lagrangian(func2_thetas):
-            grad_aug_lag = objective_gradient(func2_thetas.reshape((n, s), order='F')).flatten('F') \
-                           - c_sq_sum_alternative
-            return grad_aug_lag
+    return lagrange_multiplier
 
-        previous_thetas = current_thetas
-        current_thetas = BFGS(current_thetas, augmented_lagrangian,
-                              grad_augmented_lagrangian, current_tolerance)
 
-        if np.linalg.norm(previous_thetas - current_thetas) == global_tolerance:
-            path_figure(current_thetas.reshape((n, s), order='F'), robot)
-            return current_thetas
+def generate_augmented_lagrangian_objective(robot_arm, lagrange_multiplier, mu):
+    objective_function = generate_objective_function(robot_arm)
+    constraint_function = generate_constraints_function(robot_arm)
 
-        for j in range(2 * s):
-            current_lambdas[j] -= current_penalty * constraint(current_thetas)[j]
+    def augmented_lagrangian_objective(thetas):
+        objective = objective_function(thetas)
+        constraints = constraint_function(thetas)
+        return objective - np.sum(lagrange_multiplier * constraints) + mu / 2 * np.sum(constraints * constraints)
 
-        current_penalty *= 2
+    return augmented_lagrangian_objective
 
-        current_tolerance /= 1
 
-    return None
+def generate_augmented_lagrangian_objective_gradient(robot_arm, lagrange_multiplier, mu):
+    n = robot_arm.n
+    s = robot_arm.s
 
-# def quadratic(func1_thetas):
-#   return objective(func1_thetas.reshape((n, s), order='F')).flatten('F') + current_penalty / 2 * c_sq_sum
-# def quadratic_grad(func2_thetas):
-#   return objective_gradient(func2_thetas.reshape((n, s), order='F')).flatten('F') + current_penalty * grad_c_sq_sum
-# grad_c_sq_sum = 0
-# grad_c_sq_sum += current_constraint * current_constraint_grad
+    constraints_function = generate_constraints_function(robot_arm)
+    objective_gradient_function = generate_objective_gradient_function(robot_arm)
+    constraint_gradient_function = generate_constraint_gradients_function(robot_arm)
+
+    def augmented_lagrangian_objective_gradient(thetas):
+        constraints = constraints_function(thetas)
+        objective_gradient = objective_gradient_function(thetas)
+        constraint_gradient = constraint_gradient_function(thetas)
+
+        second_part = 0
+        for i in range(2 * s):
+            second_part += (lagrange_multiplier[i] - mu * constraints[i]) * constraint_gradient[:, i]
+
+        return objective_gradient - second_part
+
+    return augmented_lagrangian_objective_gradient
+
+
+def augmented_lagrangian_method(initial_lagrange_multiplier, initial_penalty, initial_tolerance,
+                                global_tolerance, max_iter, robot):
+    print("Starting augmented lagrangian method")
+    lagrange_multiplier_function = generate_lagrange_multiplier(robot)
+    thetas = robot.generate_initial_guess()
+    mu = initial_penalty
+    tolerance = initial_tolerance
+    lagrange_multiplier = initial_lagrange_multiplier
+    for i in range(max_iter):
+        augmented_lagrangian_objective = generate_augmented_lagrangian_objective(robot, lagrange_multiplier, mu)
+        augmented_lagrangian_objective_gradient = generate_augmented_lagrangian_objective_gradient(robot,
+                                                                                                   lagrange_multiplier,
+                                                                                                   mu)
+        thetas = BFGS(thetas, augmented_lagrangian_objective, augmented_lagrangian_objective_gradient,
+                      tolerance)
+        lagrange_multiplier = lagrange_multiplier_function(thetas, lagrange_multiplier, mu)
+        mu *= 1.5
+        tolerance *= 0.9
+    path_figure(thetas.reshape((robot.n, robot.s), order='F'), robot)
+    return thetas
+
+
+coordinates_tuple = ((1, 1, -1), (-1, 2, 3))
+lengths_tuple = (2, 3, 4, 2)
+robot = RobotArm(lengths_tuple, coordinates_tuple)
+lambdas = np.zeros(2 * robot.s)
+
+augmented_lagrangian_method(lambdas, 1, 1e-2, 1, 10, robot)
