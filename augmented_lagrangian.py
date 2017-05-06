c2da48a1e69f22d77c59fad93d80fe6d689ee5ca
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2017-05-06 20:35:22 +0200

    Made some small changes to augmented lagrangian method. Introduced convergence criterion

--- augmented_lagrangian.py
+++ augmented_lagrangian.py
@@ -5,6 +5,7 @@ from methods import BFGS
 from plotting import path_figure
 from problem import generate_objective_gradient_function, generate_objective_function
 from robot_arm import RobotArm
+import random
 
 
 def generate_lagrange_multiplier(robot_arm):
@@ -51,13 +52,18 @@ def generate_augmented_lagrangian_objective_gradient(robot_arm, lagrange_multipl
 
 
 def augmented_lagrangian_method(initial_lagrange_multiplier, initial_penalty, initial_tolerance,
-                                global_tolerance, max_iter, robot):
-    print("Starting augmented lagrangian method")
+                                global_tolerance, max_iter, robot, generate_initial_guess=True):
     lagrange_multiplier_function = generate_lagrange_multiplier(robot)
-    thetas = robot.generate_initial_guess()
+    if generate_initial_guess is True:
+        thetas = robot.generate_initial_guess()
+    elif generate_initial_guess == "random":
+        thetas = np.random.rand(robot.n*robot.s) * 2*np.pi
+    else:
+        thetas = np.zeros(robot.n * robot.s)
     mu = initial_penalty
     tolerance = initial_tolerance
     lagrange_multiplier = initial_lagrange_multiplier
+    print("Starting augmented lagrangian method")
     for i in range(max_iter):
         augmented_lagrangian_objective = generate_augmented_lagrangian_objective(robot, lagrange_multiplier, mu)
         augmented_lagrangian_objective_gradient = generate_augmented_lagrangian_objective_gradient(robot,
@@ -66,15 +72,21 @@ def augmented_lagrangian_method(initial_lagrange_multiplier, initial_penalty, in
         thetas = BFGS(thetas, augmented_lagrangian_objective, augmented_lagrangian_objective_gradient,
                       tolerance)
         lagrange_multiplier = lagrange_multiplier_function(thetas, lagrange_multiplier, mu)
-        mu *= 1.5
-        tolerance *= 0.9
-    path_figure(thetas.reshape((robot.n, robot.s), order='F'), robot)
-    return thetas
+        mu *= 1.4
+        tolerance *= 0.7
+
+        current_norm = np.linalg.norm(augmented_lagrangian_objective_gradient(thetas))
+        if current_norm < global_tolerance:
+            path_figure(thetas.reshape((robot.n, robot.s), order='F'), robot)
+            print("Augmented lagrangian method successful")
+            return thetas
+
+    print("Augmented lagrangian method unsuccessful")
 
 
-coordinates_tuple = ((1, 1, -1), (-1, 2, 3))
+coordinates_tuple = ((5, 3, -7, 9, 4), (4, 2, 3, 2, 1))
 lengths_tuple = (2, 3, 4, 2)
 robot = RobotArm(lengths_tuple, coordinates_tuple)
 lambdas = np.zeros(2 * robot.s)
 
-augmented_lagrangian_method(lambdas, 1, 1e-2, 1, 10, robot)
+augmented_lagrangian_method(lambdas, 4, 1e-2, 5e-3, 100, robot)
