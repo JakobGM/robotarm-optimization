83d5d993d1090167d340d7b452486e51a5e7a272
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2017-05-07 22:21:56 +0200

    Succesfully implemented ALM with log-barrier approach for inequality constraints

--- primal_log_barrier.py
+++ primal_log_barrier.py
@@ -2,9 +2,10 @@ import numpy as np
 
 from extended_constraints import generate_extended_constraints_gradient_function, generate_extended_constraints_function
 from extended_objective import generate_extended_objective_function, generate_extended_objective_gradient_function
-from methods import BFGS
+from methods import BFGS, gradient_descent
 from plotting import path_figure
 from robot_arm import RobotArm
+from scipy.optimize import minimize
 
 
 def generate_extended_augmented_objective(robot, barrier_penalty, lagrange_multiplier, mu):
@@ -12,8 +13,8 @@ def generate_extended_augmented_objective(robot, barrier_penalty, lagrange_multi
     extended_constraint = generate_extended_constraints_function(robot)
 
     def extended_augmented_objective(thetas_slack):
-        extended_objective(thetas_slack) - np.sum(lagrange_multiplier(thetas_slack) * extended_constraint) - mu / 2 * np.sum(
-            extended_constraint ** 2)
+        return extended_objective(thetas_slack) - np.sum(lagrange_multiplier * extended_constraint(thetas_slack)) - mu / 2 * np.sum(
+            extended_constraint(thetas_slack) ** 2)
 
     return extended_augmented_objective
 
@@ -29,7 +30,6 @@ def generate_extended_augmented_gradient(robot, barrier_penalty, lagrange_multip
         second_part = 0
         for i in range(2 * s):
             second_part += (lagrange_multiplier[i] - mu * extended_constraint(thetas_slack)[i]) * extended_constraint_gradient(thetas_slack)[:, i]
-
         return extended_objective_gradient(thetas_slack) - second_part
 
     return extended_augmented_gradient
@@ -52,11 +52,14 @@ def extended_augmented_lagrangian_method(initial_lagrange_multiplier, initial_pe
     lagrange_multiplier_function = generate_extended_lagrange_multiplier(robot)
 
     if generate_initial_guess is True:
-        thetas = robot.generate_initial_guess()
+        thetas_slack = robot.generate_initial_guess()
     elif generate_initial_guess == "random":
-        thetas = np.random.rand(robot.n * robot.s) * 2 * np.pi
+        pass
     else:
-        thetas = np.zeros(robot.n*robot.s + 2*robot.n*robot.s, )
+        thetas_slack = np.zeros(3*robot.n*robot.s, )
+        thetas_slack[:robot.n*robot.s] = np.random.rand(robot.n * robot.s) * 2 * np.pi
+        thetas_slack[robot.n*robot.s::2] = thetas_slack[:robot.n*robot.s] + robot.angular_constraint
+        thetas_slack[robot.n*robot.s+1::2] = robot.angular_constraint - thetas_slack[:robot.n*robot.s]
 
     mu = initial_penalty
     tolerance = initial_tolerance
@@ -64,7 +67,7 @@ def extended_augmented_lagrangian_method(initial_lagrange_multiplier, initial_pe
 
     # Saving the iterates if convergence analysis is active
     if convergence_analysis is True:
-        iterates = thetas.copy().reshape((robot.n * robot.s, 1))
+        iterates = thetas_slack.copy().reshape((3*robot.n*robot.s, 1))
 
     print("Starting augmented lagrangian method")
     for i in range(max_iter):
@@ -73,32 +76,35 @@ def extended_augmented_lagrangian_method(initial_lagrange_multiplier, initial_pe
         augmented_lagrangian_objective_gradient = generate_extended_augmented_gradient(robot, barrier_penalty,
                                                                                        lagrange_multiplier,
                                                                                        mu)
-        thetas = BFGS(thetas, augmented_lagrangian_objective, augmented_lagrangian_objective_gradient,
+        previous_thetas_slack = thetas_slack
+        thetas_slack = BFGS(thetas_slack, augmented_lagrangian_objective, augmented_lagrangian_objective_gradient,
                       tolerance)
-        lagrange_multiplier = lagrange_multiplier_function(thetas, lagrange_multiplier, mu)
-        mu *= 1.4
-        tolerance *= 0.7
+
+        lagrange_multiplier = lagrange_multiplier_function(thetas_slack, lagrange_multiplier, mu)
+        mu *= 5
+        tolerance *= 0.9
 
         # Adding the new thetas to iterates used for convergence analysis
         if convergence_analysis is True:
-            iterates = np.concatenate((iterates, thetas.reshape(robot.n * robot.s, 1)), axis=1)
+            iterates = np.concatenate((iterates, thetas_slack.reshape(robot.n * robot.s, 1)), axis=1)
 
-        current_norm = np.linalg.norm(augmented_lagrangian_objective_gradient(thetas))
-        if current_norm < global_tolerance:
-            path_figure(thetas.reshape((robot.n, robot.s), order='F'), robot, show=False)
+        current_norm = np.linalg.norm(augmented_lagrangian_objective_gradient(thetas_slack))
+        if current_norm < global_tolerance or np.linalg.norm(previous_thetas_slack - thetas_slack) < 0.01:
+            path_figure(thetas_slack[:robot.n*robot.s].reshape((robot.n, robot.s), order='F'), robot, show=True)
 
             print("Augmented lagrangian method successful")
             if convergence_analysis is True:
-                return iterates, generate_objective_function(robot)
+                return iterates, generate_extended_objective_function
             else:
-                return thetas
+                return thetas_slack
 
+    path_figure(thetas_slack[:robot.n * robot.s].reshape((robot.n, robot.s), order='F'), robot, show=True)
     print("Augmented lagrangian method unsuccessful")
 
 
 coordinates_tuple = ((5, 4, 6, 4, 5), (0, 2, 0.5, -2, -1))
 lengths_tuple = (3, 2, 2)
-robot = RobotArm(lengths_tuple, coordinates_tuple, angular_constraint=np.pi / 2)
+robot = RobotArm(lengths_tuple, coordinates_tuple, angular_constraint=np.pi / 8)
 lagrange_multipliers = np.zeros(2 * robot.s * (robot.n + 1))
-extended_augmented_lagrangian_method(lagrange_multipliers, 10, 1e-2, 1e-2, 100, robot, 10,
+extended_augmented_lagrangian_method(lagrange_multipliers, 0.1, 1e-2, 1e-3, 20, robot, 1e-6,
                                      generate_initial_guess=False, convergence_analysis=False)
